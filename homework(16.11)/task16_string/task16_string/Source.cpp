f8d1e1fa0129f39006ffc96017771251274d20cd
Your Name <114179591+Nazar04u@users.noreply.github.com>
2022-11-15 17:20:06 +0200

    Add "task16_string" project files

---
+++ homework(16.11)/task16_string/task16_string/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string s;
+	cin >> s;
+
+	double long f = stold(s);/*convert string to double long*/
+	cout << f << endl;
+
+	return 0;
+}
